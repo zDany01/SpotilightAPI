b8cbc7c14e5dc7a33e96c9b05da146915b2db698
zDany01 <85403430+zDany01@users.noreply.github.com>
2025-01-28 11:53:57 +0100

    Implemented Bulb color changing

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, responses
+from fastapi import FastAPI, responses, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from tinytuya import BulbDevice
+from random import randint
+import utils
+
+reset: bool = False
+red: int = randint(0, 255)
+green: int = randint(0, 255)
+blue: int = randint(0, 255)
 
 api = FastAPI(title="SpotilightAPI", description="An API that listen to predefined call to change the color and brightness of Tuya RGB Bulbs", version="0.1", license_info={"name": "MIT License", "identifier": "MIT", "url": "https://github.com/zDany01/SpotilightAPI/blob/main/LICENSE"})
-
 api.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,3 +21,17 @@ api.add_middleware(
 @api.get("/", response_class=responses.PlainTextResponse, status_code=418)
 def root():
     pass
+
+@api.head("/beat", response_class=responses.PlainTextResponse, status_code=200)
+def process_beat(bulb: BulbDevice = Depends(utils.getBulb)):
+    global reset
+    bulb.set_brightness_percentage(0 if reset else 100, True)
+    reset = not reset
+
+@api.head("/bar", response_class=responses.PlainTextResponse, status_code=200)
+def process_bar(bulb: BulbDevice = Depends(utils.getBulb)):
+    global red, green, blue
+    red = utils.randomColor(red)
+    green = utils.randomColor(green)
+    blue = utils.randomColor(blue)
+    bulb.set_colour(red, green, blue, True)
---
+++ utils.py
@@ -0,0 +1,34 @@
+from tinytuya import BulbDevice
+from os import environ
+from random import randint
+
+bulb: BulbDevice = None
+
+def getBulb() -> BulbDevice:
+    global bulb
+    if bulb is not None:
+        return bulb
+    
+    try:
+        bulbID = environ["BULB_ID"]
+        bulbIP = environ["BULB_IP"]
+        bulbSecret = environ["BULB_SECRET"]
+    except:
+        print("Unable to get Bulb Info.\nRemember to set your bulb info in the docker environment!")
+        exit(1)
+    
+    bulb = BulbDevice(
+        dev_id=bulbID,
+        address=bulbIP,
+        local_key=bulbSecret,
+        version=3.3
+    )
+
+    bulb.turn_on()
+    bulb.set_white(1000, 1000)
+    print("Accesa")
+    return bulb
+
+def randomColor(color) -> int:
+    color += randint(0, 255)
+    return color if color < 256 else color - 255
